2294b5782eb1510fe37fbeac5586c011cd4de733
Kaushal Kumar <kaushal_kumar1@persistent.com>
2024-02-22 14:07:31 +0530

    added git-demo project to local repo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public void addData(String data){
+        System.out.println("data - "+data);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+    }
+}
